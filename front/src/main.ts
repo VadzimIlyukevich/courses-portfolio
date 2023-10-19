32b0d0dd0e3d20fbdd2bdb78114a58ac0598d968
v.ilyukevich <v.ilyukevich@rit.va>
2023-10-19 20:47:08 +0300

    connect to libs

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from "./routers";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router).mount("body");
---
+++ front/src/pages/MainPage.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+name: "MainPage"
+}
+</script>
+
+<style scoped>
+
+</style>
