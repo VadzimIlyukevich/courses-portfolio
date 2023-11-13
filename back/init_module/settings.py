766bbab1d036f9ecaa3f55746db3bf186a453d73
Vadzim <mr.vadikru@mail.ru>
2023-11-13 22:27:19 +0300

    conflict

--- back/init_module/settings.py
+++ back/init_module/settings.py
@@ -128,4 +128,4 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.users.BigAutoField"
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
